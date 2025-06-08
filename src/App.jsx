ecf91461baa37a0edcbfe9939725de703f207af4
kcodweb <kmatrb0@gmail.com>
2025-06-08 17:14:45 +0530

    hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import ResourcesPage from './pages/ResourcesPage';
